65abf1a7e001fa4afb75e65d548a828391822969
Daniel Motejadded <98238532+Incarn4ti0n@users.noreply.github.com>
2024-03-06 10:35:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -168,7 +168,7 @@ const loadImg = function (entries, observer) {
 
 const imgObserver = new IntersectionObserver(loadImg, {
   root: null,
-  threshold: 1,
+  threshold: 0.5,
   rootMargin: '200px',
 });
 
